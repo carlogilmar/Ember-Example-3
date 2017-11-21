7f46343db8565ab3f9a4347cdb1c5083dc92b484
carlogilmar <carlogilmar12@gmail.com>
2017-11-21 16:48:13 -0600

    Sending the action for save in route payment-info

--- app/routes/checkout/payment-info.js
+++ app/routes/checkout/payment-info.js
@@ -1,8 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  controllerName: 'checkout',
+
+  //controllerName: 'checkout',
   model() {
     return this.modelFor('checkout');
+  },
+
+  actions: {
+    submitForm() {
+			alert("okas");
+      //this.transitionToRoute('checkout.confirmation');
+    }
   }
+
 });
